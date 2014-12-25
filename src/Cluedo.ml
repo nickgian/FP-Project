7c422feec45e55ff8080d40704b9f9c5e7f6c210
gian <nick.giannarakis@gmail.com>
2014-12-25 15:00:31 +0200

    Cluedo?

--- src/Cluedo.ml
+++ src/Cluedo.ml
@@ -7,7 +7,19 @@ type suspect = Alice | Bob
 
 type weapon = Gun | Pipe
 
-let whodunnit : suspect mon = failwith "TODO"
+
+(*A = 0.3 * 0.97, 0.3 * 0.03,  B = 0.7 * 0.2, 0.7 * 0.8*)
+
+let s = distr [(Alice, 0.3); (Bob, 0.7)]
+
+let whodunnit : suspect mon =
+  s >>= fun p ->
+  match p with
+    | Alice ->
+      distr [(Pipe, 0.97); (Gun, 0.03)] >>= fun w -> ret Alice
+    | Bob ->
+      distr [(Pipe, 0.2); (Gun, 0.8)] >>= fun w -> ret Bob
+  
 
 let print_suspect = function
   | Alice -> printf "Alice"
